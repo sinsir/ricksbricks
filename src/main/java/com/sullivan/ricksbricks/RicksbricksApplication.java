ebad37d806183da2cc377085fed4f21ed60ba0cb
Gareth Sullivan <sullivan.gareth@gmail.com>
2023-05-11 20:39:16 +0100

    Initial check in of project created using https://start.spring.io/

---
+++ src/main/java/com/sullivan/ricksbricks/RicksbricksApplication.java
@@ -0,0 +1,13 @@
+package com.sullivan.ricksbricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicksbricksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicksbricksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sullivan/ricksbricks/RicksbricksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sullivan.ricksbricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicksbricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
