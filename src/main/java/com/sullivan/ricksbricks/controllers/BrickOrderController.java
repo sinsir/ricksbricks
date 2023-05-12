ae5310a37a1c921b9d9dedba334b1f2ac3668b1d
Gareth Sullivan <sullivan.gareth@gmail.com>
2023-05-12 08:17:18 +0100

    OpenAPI documentation added

--- src/main/java/com/sullivan/ricksbricks/controllers/BrickOrderController.java
+++ src/main/java/com/sullivan/ricksbricks/controllers/BrickOrderController.java
@@ -1,6 +1,8 @@
 package com.sullivan.ricksbricks.controllers;
 
 import com.sullivan.ricksbricks.service.BrickOrderingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,11 @@ public class BrickOrderController {
 
     @PostMapping("{numberOfBricks}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Submit new orders for bricks",
+            description = "Submit new orders for bricks to start customers’ orders",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Brick order added succesfully")
+            })
     public Map<String, Integer> addBrickOrder(@PathVariable("numberOfBricks") int numberOfBricks) {
 
         return Map.of("orderReference", brickOrderingService.addBrickOrder(numberOfBricks));
